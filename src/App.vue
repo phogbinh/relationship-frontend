1cc883093de5c06985b4c4f0a4ac9ed6441e64ec
bill <phongbinh2511@gmail.com>
2021-09-12 23:52:32 +0800

    [master] Emptied the project.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id='app'>
+    <Search/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Search from './components/Search.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  'name': 'App',
+  'components': {
+    Search
   }
 }
 </script>
---
+++ src/components/Search.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class='search'>
+  </div>
+</template>
+
+<script>
+export default {
+  'name': 'Search'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
