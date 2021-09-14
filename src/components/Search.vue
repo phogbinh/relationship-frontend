54a71397645738467878fdb6d5e905f79960dfe1
bill <phongbinh2511@gmail.com>
2021-09-14 12:53:35 +0800

    [master] Added nickname search box.

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,11 +1,17 @@
 <template>
   <div class='search'>
+    <input type='text' placeholder='Enter the nickname of the dude that you want to search..' v-model='nickname'>
   </div>
 </template>
 
 <script>
 export default {
-  'name': 'Search'
+  'name': 'Search',
+  'data': function() {
+    return {
+      'nickname': ''
+    }
+  }
 }
 </script>
 
